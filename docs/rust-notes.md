6bc64ffb506b0469154aa84276fc4f95372fe34a
Doug Russell <doug@therussellfamily.org>
2025-09-01 14:59:27 -0400

    I'm  just frustrated.

--- docs/rust-notes.md
+++ docs/rust-notes.md
@@ -17,3 +17,27 @@ $ cargo new new-project
 ```
 $ cargo run
 ```
+
+<a name="datatypes"></a>
+## Datatypes
+- Statically Typed Language
+- Scalar
+    - Integer
+| Length | Signed | Unsigned | Range |
+|------|------|--------|-----|
+| 8-bit | i8 | u8 | -128 to 127 or 0 to 255 |
+| 16-bit | i16 | u16 | -32,768 to 32,767 or 0 to 65,535 |
+| 32-bit | i32 | u32 | -2,147,483,648 to 2,147,483,647 or 0 to 4,294,967,295 |
+| 64-bit | i64 | u64 | -9,223,372,036,854,775,808 to 9,223,372,036,854,775,807 or 0 to 18,446,744,073,709,551,615 |
+
+    - Float
+    | Length | Signed | Unsigned |
+    | ------ | ------ | -------- |
+    | 32-bit | f32 | f64 |
+    | 64-bit | f32 | f64 |
+    
+    - Boolean
+    - Character
+- Compound
+    - Tuple
+    - Arrays
--- zenva/variables/src/main.rs
+++ zenva/variables/src/main.rs
@@ -1,3 +1,32 @@
 fn main() {
-    println!("Hello, world!");
+    let mut value = 9;
+    value = 10;
+    println!("The value is {}", value);
+
+    let x = 64;
+    let x = x+1; // Shadowing
+    println!("The value of x is {}", x);
+
+    let x = "RUST PROGRAMMING";
+    println!("The value of x is {}", x);
+
+    let small_value:i8 = 100;
+    let sample_float:f32 = -700.25;
+
+    println!("Small int {}", small_value);
+    println!("Sample float {}", sample_float);
+
+    // Arrays
+    let numbers = [1, 2, 3, 4, 5];
+    println!("Element at index 0: {}", numbers[0]);
+
+    // Tuples
+    let person = ("Alice", 30, 5.4);
+    println!("Name:   {}", person.0);
+    println!("Age:    {} ", person.1);
+    println!("Height: {}", person.2);
+
+    // Constants
+    const PI:f32 = 3.14;
+    println!("Value of pi {}", PI);
 }
